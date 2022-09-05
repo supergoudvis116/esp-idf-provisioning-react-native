90e1b151ae99f1a599de45e3ac5ecd0a7ac16f92
pikann <hvhai22@gmail.com>
2022-09-05 09:31:53 +0700

    feat: implement ESP IDF provisioning for IOS

--- ios/EspIdfProvisioningReactNative.m
+++ ios/EspIdfProvisioningReactNative.m
@@ -2,9 +2,14 @@
 
 @interface RCT_EXTERN_MODULE(EspIdfProvisioningReactNative, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(create)
+RCT_EXTERN_METHOD(scanBleDevices:(NSString *)prefix resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(connectToBLEDevice:(NSString *)name resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getProofOfPossession:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(setProofOfPossession:(NSString *)proof)
+RCT_EXTERN_METHOD(scanNetworks:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(provisionNetwork:(NSString *)ssid password:(NSString *)password resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(sendCustomData:(NSString *)customEndPoint customData:(NSString *)customData resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/EspIdfProvisioningReactNative.swift
+++ ios/EspIdfProvisioningReactNative.swift
@@ -1,8 +1,176 @@
-@objc(EspIdfProvisioningReactNative)
-class EspIdfProvisioningReactNative: NSObject {
+import Foundation
+import ESPProvision
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+@objc(EspIdfProvisioningReactNative)
+class EspIdfProvisioningReactNative: NSObject, ESPDeviceConnectionDelegate {
+    private var provisionManager: ESPProvisionManager!
+    private var listDevicesByUuid: [String: ESPDevice] = [:]
+    private var espDevice: ESPDevice!
+    private var deviceConnected = false
+    private var proofOfPossession = ""
+
+    @objc
+    func create() -> Void {
+        self.provisionManager = ESPProvisionManager.shared
+        print("Create method")
+    }
+    
+    @objc(scanBleDevices:resolve:reject:)
+    func scanBleDevices(
+        prefix: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) -> Void {
+        print("Scan started")
+        ESPProvisionManager.shared.searchESPDevices(
+            devicePrefix:prefix,
+            transport:.ble,
+            security:.secure
+        ) { deviceList, error in
+            if let error = error {
+                reject("SCAN_ERROR", "Error when scan devices", error)
+            } else {
+                let response: NSMutableArray = []
+                if let list = deviceList {
+                    for device in list {
+                        print("device: \(device.name)")
+                        self.listDevicesByUuid[device.name] = device
+                        response.add([
+                            "name": device.name,
+                            "serviceUuid": device.name
+                        ])
+                    }
+                }
+                resolve(response)
+            }
+        }
+    }
+    
+    @objc(connectToBLEDevice:resolve:reject:)
+    func connectToBLEDevice(
+        name: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) -> Void {
+        if let device = listDevicesByUuid[name] {
+            device.disconnect()
+            
+            device.connect(delegate: self) { status in
+                switch status {
+                    case .connected:
+                        print("Device Connected Event Received")
+                        self.espDevice = device
+                        self.deviceConnected = true
+                        let response: NSDictionary = ["success": true]
+                        resolve(response)
+                        break
+                    case let .failedToConnect(error):
+                        self.espDevice = nil
+                        self.deviceConnected = false
+                        print("error: \(error.localizedDescription)")
+                        reject("Connection error", "Connection error", error)
+                        break
+                    default:
+                        self.espDevice = nil
+                        self.deviceConnected = false
+                        print("Device disconnected")
+                        reject("Device disconnected", "Please check the connection", NSError())
+                        break
+                }
+            }
+        }
+        else {
+            reject("Device don't exists", "Please scan BLE devices first", NSError(domain: "", code: 200, userInfo: nil))
+        }
+    }
+    
+    func getProofOfPossesion(forDevice: ESPDevice, completionHandler: @escaping (String) -> Void) {
+        completionHandler(self.proofOfPossession)
+    }
+    
+    @objc
+    func getProofOfPossession(
+        _ resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        resolve(self.proofOfPossession)
+    }
+    
+    @objc
+    func setProofOfPossession(_ proof: String) {
+        self.proofOfPossession = proof
+    }
+    
+    @objc
+    func scanNetworks(
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        if (!self.deviceConnected) {
+            print("No device connected")
+            reject("No device connected", "Please connect a device first", NSError(domain: "", code: 200, userInfo: nil))
+            return
+        }
+        
+        self.espDevice.scanWifiList(completionHandler: {wifiList, error in
+            if let error = error {
+                reject("Network Scan Error", "WiFi networks scan has failed", error)
+            } else if let wifiList = wifiList {
+                let response: NSMutableArray = []
+                for item in wifiList {
+                    response.add([
+                        "ssid": item.ssid
+                    ])
+                }
+                resolve(response)
+            } else {
+                resolve([])
+            }
+        })
+    }
+    
+    @objc(provisionNetwork:password:resolve:reject:)
+    func provisionNetwork(
+        ssid: String,
+        password: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.espDevice.provision(ssid: ssid,
+                                 passPhrase: password,
+                                 completionHandler: {status in
+                                    switch status {
+                                        case .configApplied:
+                                            print("Wi-Fi credentials successfully applied to the device")
+                                            break
+                                        case .success:
+                                            print("Device is provisioned successfully")
+                                            let response: NSDictionary = ["success": true]
+                                            resolve(response)
+                                            break
+                                        case let .failure(error):
+                                            print("Provisioning is failed")
+                                            reject("Error in provision listener",
+                                                        "Provisioning is failed", error);
+                                            break
+                                    }
+                                })
+    }
+    
+    @objc
+    func sendCustomData(
+        customEndPoint: String,
+        customData: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.espDevice.sendData(path: customEndPoint, data: customData.data(using: .utf16)!, completionHandler: {data, error in
+            if let error = error {
+                print("Custom data provision has failed")
+                reject("Custom data provision has failed", "Custom data provision has failed", error)
+            }
+            let response: NSDictionary = ["success": true, "data": String(describing: data)]
+            resolve(response)
+        })
+    }
 }
